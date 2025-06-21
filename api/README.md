fe8eeefc0bf3aafed5e9300d5a6b6d2eb962925e
Artur Słomowski <koder@MacBook-Pro-Artur.local>
2025-06-21 09:07:40 +0200

    begining of cart logic

---
+++ api/README.md
@@ -0,0 +1,5 @@
+# Setup
+1. create venv via `python3 -m venv  venv`
+2. activate venv via `source venv/bin/activate`
+3. install requirements via `pip install -r requirements.txt`
+4. run the app via `python3.11 src/main.py`
---
+++ api/src/agents/cart/cartState.py
@@ -0,0 +1,18 @@
+
+from decimal import Decimal
+from typing import Annotated, Sequence, TypedDict
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+from pydantic import BaseModel, Field
+
+class ProductSchema(BaseModel): # To jest odpowiednik TypedDict, ale z walidacją
+    product_id: Annotated[str, Field(min_length=6)]
+    name: Annotated[str, Field(min_length=3)]
+    price: Annotated[Decimal, Field(gt=Decimal('0.00'))]
+    quantity: Annotated[int, Field(gt=0)]
+    img_url: Annotated[str, Field(min_length=1)]
+
+class ChatState(BaseModel):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    cartItems: list[ProductSchema] 
+    cartTotal: Annotated[Decimal, Field(gt=Decimal('0.00'))]
