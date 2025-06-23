547c3c36182f20167346debae52f60cd18d12725
Artur Słomowski <artur.slomowski@gmail.com>
2025-06-23 22:30:17 +0200

    beging of add new product mechanic

---
+++ api/src/agents/addProduct/add_product_state.py
@@ -0,0 +1,15 @@
+
+from decimal import Decimal
+from typing import Annotated
+from pydantic import BaseModel, Field
+
+
+class AddProductState(BaseModel):
+    product_id: Annotated[str, Field(min_length=6)]
+    name: Annotated[str, Field(min_length=3)]
+    price: Annotated[Decimal, Field(gt=Decimal('0.00'))]
+    quantity: Annotated[int, Field(gt=0)]
+    img_url: Annotated[str, Field(min_length=1)]
+    description: Annotated[str, Field(min_length=1)] = ""
+    short_description: Annotated[str, Field(min_length=1)] = ""
+    category: Annotated[str, Field(min_length=1)] = ""
