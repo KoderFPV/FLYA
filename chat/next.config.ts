0e800bdc082f0ef6021f696f0df1df86bba55f40
Artur Słomowski <koder@MacBook-Pro-Artur.local>
2025-07-15 15:49:14 +0300

    api rewrite

--- chat/next.config.ts
+++ chat/next.config.ts
@@ -1,17 +1,18 @@
 import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 const nextConfig: NextConfig = {
   devIndicators: false,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/chat',
-  //       permanent: false,
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${API_URL}/:path*`,
+      },
+    ];
+  },
   sassOptions: {
     prependData: `
         @import 'src/styles/variables.module.scss';
--- chat/src/services/fetch.service.ts
+++ chat/src/services/fetch.service.ts
@@ -2,20 +2,15 @@ import { showError } from "./errors.service";
 
 const debug = true;
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-if (!API_URL) {
-  throw new Error("BASE_URL environment variable is not set");
-}
 
 export const appFetch = async (pathWithSlashAthBeggning: string, options?: RequestInit): Promise<string | null> => {
   if (debug) {
-    console.log("Fetch URL:", `${API_URL}${pathWithSlashAthBeggning}`);
+    console.log("Fetch URL:", `/api/${pathWithSlashAthBeggning}`);
     console.log("Fetch Options:", options);
   }
 
   try {
-    const response = await fetch(`${API_URL}${pathWithSlashAthBeggning}`, options);
+    const response = await fetch(`/api/${pathWithSlashAthBeggning}`, options);
 
 
     const body = await response.text();
@@ -30,12 +25,12 @@ export const appFetch = async (pathWithSlashAthBeggning: string, options?: Reque
 
 export const appFetchStream = async (pathWithSlashAthBeggning: string, options?: RequestInit): Promise<Response> => {
   if (debug) {
-    console.log("Fetch Stream URL:", `${API_URL}${pathWithSlashAthBeggning}`);
+    console.log("Fetch Stream URL:", `/api/${pathWithSlashAthBeggning}`);
     console.log("Fetch Stream Options:", options);
   }
 
   try {
-    const response = await fetch(`${API_URL}${pathWithSlashAthBeggning}`, options);
+    const response = await fetch(`/api/${pathWithSlashAthBeggning}`, options);
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
