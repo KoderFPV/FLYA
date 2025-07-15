0aa5a5b4b45428c4d189ee697112a010eb00eb6e
Artur Słomowski <koder@MacBook-Pro-Artur.local>
2025-07-15 21:32:35 +0300

    change api name env

--- chat/next.config.ts
+++ chat/next.config.ts
@@ -1,7 +1,7 @@
 import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = process.env.API_URL;
 
 const nextConfig: NextConfig = {
   devIndicators: false,
